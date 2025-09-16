430f4d7b41fa7e0a5d44df455b50e9666262dea1
Soulless9801 <jasonli2026@gmail.com>
2025-09-16 16:15:14 -0500

    hi

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFirstMAUIMobileApp
+﻿using MyFirstMAUIMobileApp.Views;
+
+namespace MyFirstMAUIMobileApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
     }
 }
