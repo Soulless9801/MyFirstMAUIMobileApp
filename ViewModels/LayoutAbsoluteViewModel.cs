1356012eec6e18dc6966bb66e653bfc391c2d202
Soulless9801 <jasonli2026@gmail.com>
2025-10-30 16:15:25 -0500

    minor tweaks

--- ViewModels/LayoutAbsoluteViewModel.cs
+++ ViewModels/LayoutAbsoluteViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using MyFirstMAUIMobileApp.Models.Titles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyFirstMAUIMobileApp.ViewModels
 {
